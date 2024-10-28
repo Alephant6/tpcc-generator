c3800fa907fba17b92ca5a3b48e3a1704ee5b2d3
sqiang <sqiang@buffalo.edu>
2024-10-28 18:21:35 -0400

    init

--- TpccGenerator.cpp
+++ TpccGenerator.cpp
@@ -9,6 +9,117 @@
 
 using namespace std;
 
+enum TableID {
+  WAREHOUSE = 0,
+  DISTRICT,
+  CUSTOMER,
+  NEWORDER,
+  ORDER,
+  ITEM,
+  STOCK,
+  ORDERLINE,
+  HISTORY,
+  ORDERSTATUS
+};
+
+// Union for encoding TPCC keys
+union TPCCKey {
+  int64_t db_key;
+  uint64_t alt_db_key;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t payload : 60;
+  } tab;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+  } warehouse;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+    uint64_t d_id : 4;
+  } district;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+    uint64_t d_id : 4;
+    uint64_t c_id : 12;
+  } customer;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+    uint64_t d_id : 4;
+    uint64_t o_id : 12;
+  } neworder, order;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+    uint64_t d_id : 4;
+    uint64_t o_id : 12;
+    uint64_t ol_number : 4;
+  } orderline;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t i_id : 17;
+  } item;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+    uint64_t i_id : 17;
+  } stock;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t client_id : 14;
+    uint64_t h_id : 40;
+  } history;
+
+  struct {
+    uint64_t tabid : 4;
+    uint64_t w_id : 14;
+    uint64_t d_id : 4;
+    uint64_t c_id : 12;
+    uint64_t o_id : 12;
+  } orderstatus;
+};
+
+void EncodeDistrictKey(int64_t* db_key, uint64_t w_id, uint64_t d_id) {
+    TPCCKey * key = (TPCCKey *) db_key;
+    key->district.tabid = DISTRICT; 
+    key->district.w_id = w_id;
+    key->district.d_id = d_id;
+}
+
+void EncodeCustomerKey(int64_t* db_key, uint64_t w_id, uint64_t d_id, uint64_t c_id) {
+    TPCCKey* key = (TPCCKey*) db_key;
+    key->customer.tabid = CUSTOMER; 
+    key->customer.w_id = w_id;
+    key->customer.d_id = d_id;
+    key->customer.c_id = c_id;
+}
+
+void EncodeItemKey(int64_t* db_key, uint64_t i_id) {
+    TPCCKey* key = (TPCCKey*) db_key;
+    key->item.tabid = ITEM;  
+    key->item.i_id = i_id;
+}
+
+void EncodeStockKey(int64_t* db_key, uint64_t w_id, uint64_t i_id) {
+    TPCCKey* key = (TPCCKey*) db_key;
+    key->stock.tabid = STOCK;  
+    key->stock.w_id = w_id;
+    key->stock.i_id = i_id;
+}
+
+
 TpccGenerator::TpccGenerator(int64_t warehouse_count, const string &folder)
         : warehouse_count(warehouse_count)
           , folder(folder)
@@ -51,16 +162,9 @@ void TpccGenerator::generateDistricts()
 {
    cout << "Generating districts .. " << flush;
 
+   int64_t db_key;
    int64_t d_id;
    int64_t d_w_id;
-   array<char, 10> d_name = {};
-   array<char, 20> d_street_1 = {};
-   array<char, 20> d_street_2 = {};
-   array<char, 20> d_city = {};
-   array<char, 2> d_state = {};
-   array<char, 9> d_zip = {};
-   float d_tax;
-   float d_ytd;
    int64_t d_next_o_id;
 
    csv::CsvWriter d_csv(folder + "/district.csv");
@@ -68,15 +172,11 @@ void TpccGenerator::generateDistricts()
    // Each warehouse has DIST_PER_WARE (10) districts
    for (d_w_id = 1; d_w_id<=warehouse_count; d_w_id++) {
       for (d_id = 1; d_id<=kDistrictsPerWarehouse; d_id++) {
-         d_ytd = 30000.0;
-         d_next_o_id = 3001L;
-         makeAlphaString(6L, 10L, d_name.data());
-         makeAddress(d_street_1.data(), d_street_2.data(), d_city.data(), d_state.data(), d_zip.data());
-         d_tax = ((float) makeNumber(10L, 20L)) / 100.0f;
+         EncodeDistrictKey(&db_key, d_w_id, d_id);
+         d_next_o_id = 1;
 
          // @formatter:off
-         d_csv << d_id << d_w_id << d_name << d_street_1 << d_street_2 << d_city << d_state << d_zip << csv::Precision(4)
-               << d_tax << csv::Precision(2) << d_ytd << d_next_o_id << csv::endl;
+         d_csv << db_key << d_next_o_id << csv::endl;
          // @formatter:on
       }
    }
@@ -88,63 +188,26 @@ void TpccGenerator::generateCustomerAndHistory()
 {
    cout << "Generating customers and their history .. " << flush;
 
+   int64_t db_key;
    int64_t c_id;
    int64_t c_d_id;
    int64_t c_w_id;
-   array<char, 16> c_first = {};
-   array<char, 2> c_middle = {};
-   array<char, 16> c_last = {};
-   array<char, 20> c_street_1 = {};
-   array<char, 20> c_street_2 = {};
-   array<char, 20> c_city = {};
-   array<char, 2> c_state = {};
-   array<char, 9> c_zip = {};
-   array<char, 16> c_phone = {};
-   array<char, 15> c_since = {}; // XXX used in history and customer and generated over and over again
-   array<char, 2> c_credit = {};
-   float c_credit_lim;
-   float c_discount;
-   float c_balance;
+   int64_t c_balance;
    array<char, 500> c_data = {};
    float h_amount;
    array<char, 24> h_data = {};
 
    csv::CsvWriter c_csv(folder + "/customer.csv");
-   csv::CsvWriter h_csv(folder + "/history.csv");
 
    // Each warehouse has DIST_PER_WARE (10) districts
    for (c_w_id = 1; c_w_id<=warehouse_count; c_w_id++) {
       for (c_d_id = 1; c_d_id<=kDistrictsPerWarehouse; c_d_id++) {
          for (c_id = 1; c_id<=kCustomerPerDistrict; c_id++) {
-            makeAlphaString(8, 16, c_first.data());
-            c_middle[0] = 'O';
-            c_middle[1] = 'E';
-            if (c_id<=1000)
-               makeLastName(c_id - 1, c_last.data());
-            else
-               makeLastName(makeNonUniformRandom(255, 0, 999), c_last.data());
-            makeAddress(c_street_1.data(), c_street_2.data(), c_city.data(), c_state.data(), c_zip.data());
-            makeNumberString(16, 16, c_phone.data());
-            c_credit[0] = makeNumber(0L, 1L) == 0 ? 'G' : 'B';
-            c_credit[1] = 'C';
-            c_credit_lim = 50000;
-            c_discount = ((float) makeNumber(0L, 50L)) / 100.0f;
-            c_balance = -10.0f;
-            makeNow(c_since.data());
-            makeAlphaString(300, 500, c_data.data());
+            EncodeCustomerKey(&db_key, c_w_id, c_d_id, c_id);
+            c_balance = -1000;
 
             // @formatter:off
-            c_csv << c_id << c_d_id << c_w_id << c_first << c_middle << c_last << c_street_1 << c_street_2 << c_city
-                  << c_state << c_zip << c_phone << c_since << c_credit << csv::Precision(2) << c_credit_lim
-                  << csv::Precision(4) << c_discount << csv::Precision(2) << c_balance << 10.0f << int64_t(1)
-                  << int64_t(0) << c_data << csv::endl;
-            // @formatter:on
-
-            h_amount = 10.0;
-            makeAlphaString(12, 24, h_data.data());
-
-            // @formatter:off
-            h_csv << c_id << c_d_id << c_w_id << c_d_id << c_w_id << c_since << h_amount << h_data << csv::endl;
+            c_csv << db_key << c_balance << csv::endl;
             // @formatter:on
          }
       }
@@ -157,43 +220,18 @@ void TpccGenerator::generateItems()
 {
    cout << "Generating items .. " << flush;
 
+   int64_t db_key;
    int64_t i_id;
-   array<char, 24> i_name = {};
-   float i_price;
-   array<char, 50> i_data = {};
-   uint32_t i_data_size;
-   vector<bool> orig(kItemCount, false);
-   int64_t i_im_id;
+   int64_t i_price;
 
    csv::CsvWriter i_csv(folder + "/item.csv");
 
-   for (uint32_t i = 0; i<kItemCount / 10; i++) {
-      uint32_t pos;
-      do {
-         pos = makeNumber(0L, kItemCount - 1);
-      } while (orig[pos]);
-      orig[pos] = true;
-   }
-
    for (i_id = 1; i_id<=kItemCount; i_id++) {
-      makeAlphaString(14, 24, i_name.data());
-      i_price = ((float) makeNumber(100L, 10000L)) / 100.0f;
-      i_data_size = makeAlphaString(26, 50, i_data.data());
-      i_im_id = makeNumber(0, 10000);
-      if (orig[i_id]) {
-         uint32_t pos = makeNumber(0L, i_data_size - 8);
-         i_data[pos] = 'o';
-         i_data[pos + 1] = 'r';
-         i_data[pos + 2] = 'i';
-         i_data[pos + 3] = 'g';
-         i_data[pos + 4] = 'i';
-         i_data[pos + 5] = 'n';
-         i_data[pos + 6] = 'a';
-         i_data[pos + 7] = 'l';
-      }
+      EncodeItemKey(&db_key, i_id);
+      i_price = makeNumber(100L, 10000L);
 
       // @formatter:off
-      i_csv << i_id << i_im_id << i_name << csv::Precision(2) << i_price << i_data << csv::endl;
+      i_csv << db_key << i_price << csv::endl;
       // @formatter:on
    }
 
@@ -204,67 +242,24 @@ void TpccGenerator::generateStock()
 {
    cout << "Generating stocks .. " << flush;
 
+   int64_t db_key;
    int64_t s_i_id;
    int64_t s_w_id;
    int64_t s_quantity;
-   array<char, 24> s_dist_01 = {};
-   array<char, 24> s_dist_02 = {};
-   array<char, 24> s_dist_03 = {};
-   array<char, 24> s_dist_04 = {};
-   array<char, 24> s_dist_05 = {};
-   array<char, 24> s_dist_06 = {};
-   array<char, 24> s_dist_07 = {};
-   array<char, 24> s_dist_08 = {};
-   array<char, 24> s_dist_09 = {};
-   array<char, 24> s_dist_10 = {};
    int64_t s_ytd = 0;
    int64_t s_order_cnt = 0;
    int64_t s_remote_cnt = 0;
-   array<char, 50> s_data = {};
-   vector<bool> orig(kItemCount, false);
 
    csv::CsvWriter s_csv(folder + "/stock.csv");
 
    for (s_w_id = 1; s_w_id<=warehouse_count; s_w_id++) {
-      orig.assign(kItemCount, false);
-
-      for (uint32_t i = 0; i<kItemCount / 10; i++) {
-         int64_t pos;
-         do {
-            pos = makeNumber(0L, kItemCount - 1);
-         } while (orig[pos]);
-         orig[pos] = 1;
-      }
-
       for (s_i_id = 1; s_i_id<=kItemCount; s_i_id++) {
+        EncodeStockKey(&db_key, s_w_id, s_i_id);
          s_quantity = makeNumber(10L, 100L);
-         makeAlphaString(24, 24, s_dist_01.data());
-         makeAlphaString(24, 24, s_dist_02.data());
-         makeAlphaString(24, 24, s_dist_03.data());
-         makeAlphaString(24, 24, s_dist_04.data());
-         makeAlphaString(24, 24, s_dist_05.data());
-         makeAlphaString(24, 24, s_dist_06.data());
-         makeAlphaString(24, 24, s_dist_07.data());
-         makeAlphaString(24, 24, s_dist_08.data());
-         makeAlphaString(24, 24, s_dist_09.data());
-         makeAlphaString(24, 24, s_dist_10.data());
-         uint32_t s_data_size = makeAlphaString(26, 50, s_data.data());
-         if (orig[s_i_id]) {
-            int64_t pos = makeNumber(0L, s_data_size - 8);
-            s_data[pos] = 'o';
-            s_data[pos + 1] = 'r';
-            s_data[pos + 2] = 'i';
-            s_data[pos + 3] = 'g';
-            s_data[pos + 4] = 'i';
-            s_data[pos + 5] = 'n';
-            s_data[pos + 6] = 'a';
-            s_data[pos + 7] = 'l';
-         }
 
          // @formatter:off
-         s_csv << s_i_id << s_w_id << s_quantity << s_dist_01 << s_dist_02 << s_dist_03 << s_dist_04 << s_dist_05
-               << s_dist_06 << s_dist_07 << s_dist_08 << s_dist_09 << s_dist_10 << s_ytd << s_order_cnt
-               << s_remote_cnt << s_data << csv::endl;
+         s_csv << db_key << s_quantity << s_order_cnt << s_ytd 
+               << s_remote_cnt << csv::endl;
          // @formatter:on
       }
    }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include "TpccGenerator.hpp"
 
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+namespace fs = std::filesystem;
+
+void combineFiles(const std::string& directoryPath, const std::string& outputFileName) {
+    std::string outputFilePath = directoryPath + "/" + outputFileName;
+
+    std::vector<std::filesystem::path> files;
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
+        if (entry.is_regular_file() && entry.path().filename() != outputFileName) {
+            files.push_back(entry.path());
+        }
+    }
+
+    std::ofstream outputFile(outputFilePath, std::ios::trunc);
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return;
+    }
+
+    for (const auto& filePath : files) {
+        std::ifstream inputFile(filePath);
+        if (inputFile.is_open()) {
+            outputFile << inputFile.rdbuf();  
+            inputFile.close();
+        } else {
+            std::cerr << "Failed to open file: " << filePath << std::endl;
+        }
+    }
+    outputFile.close();
+}
 
 int main(int argc, char **argv)
 {
@@ -25,13 +59,15 @@ int main(int argc, char **argv)
    TpccGenerator generator((uint32_t) warehouse_count, argv[2]);
 
    cout << "I am loading TPCC data for " << warehouse_count << " warehouse" << (warehouse_count!=1 ? "s" : "") << ", hold on .." << endl << endl;
-   generator.generateWarehouses();
+  //  generator.generateWarehouses();
    generator.generateDistricts();
    generator.generateCustomerAndHistory();
    generator.generateItems();
    generator.generateStock();
-   generator.generateOrdersAndOrderLines();
+  //  generator.generateOrdersAndOrderLines();
    cout << endl << ".. data generation completed successfully :)" << endl;
+
+   combineFiles(argv[2], "tpcc_table.csv");
 
    return 0;
 }
